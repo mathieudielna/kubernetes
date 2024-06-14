822d879127264278f432787deb8576692f6388f3
Mathieu Dielna <mathieudielna@MBP-von-Mathieu.lan>
2024-06-15 01:35:05 +0200

    update config

--- init.sh
+++ init.sh
@@ -1,18 +1,27 @@
-# Construire les images Docker "si nécessaire"
-# docker build -t angular-web:latest ./front
-# docker build -t mathieudielna/front-kubernetes:latest .
-# docker build -t back_node:latest ./api
+#!/bin/bash
 
-# Supprimer toutes les ressources Kubernetes, si nécessaire (commenté pour la sécurité)
+# Construire les images Docker si nécessaire
+docker build -t angular-web:latest ./frontend
+docker build -t back_node:latest ./backend
+
+# Supprimer toutes les ressources Kubernetes (commenté pour la sécurité)
 # kubectl delete all --all --all-namespaces
 
 # Appliquer les configurations Kubernetes
-kubectl apply -f ./secret.yml  # Appliquer le fichier de secrets en premier
+# kubectl apply -f ./secret.yml  # Appliquer si vous avez des secrets à déployer
 kubectl apply -f ./mongodb.yml
 kubectl apply -f ./angular.yml
 kubectl apply -f ./node.yml
 
-
 # Vérifier les déploiements et services
-kubectl get all
+echo "---------------------------"
+echo "Vérification des déploiements :"
+kubectl get deployments
+
+echo "---------------------------"
+echo "Vérification des pods :"
+kubectl get pods
+
+echo "---------------------------"
+echo "Vérification des services :"
 kubectl get services
